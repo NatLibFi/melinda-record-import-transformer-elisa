b1472aeac002f1d41e7aeed56ef7f086a9458df8
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-09-28 13:05:06 +0300

    Add stuff

--- src/transform/convert/index.js
+++ src/transform/convert/index.js
@@ -48,7 +48,8 @@ export default ({isLegalDeposit, sources, sender, moment = momentOrig}) => ({Pro
     throw new Error('  No data source found.');
   }
 
-  // Console.log('      isLegalDeposit = ', isLegalDeposit);
+  
+  //    Console.log('      isLegalDeposit = ', isLegalDeposit);
 
   checkSupplierData();
 
